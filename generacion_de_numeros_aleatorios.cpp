49621ba3bf99aa10209667ff7290a1cb1dc1cf8b
Ricardofloressgit <155786348+Ricardofloressgit@users.noreply.github.com>
2024-01-17 20:50:34 -0400

    Add files via upload

---
+++ generacion_de_numeros_aleatorios.cpp
@@ -0,0 +1,68 @@
+/*Titulo :ejercicio en clases	
+Autor : Ricardo flores soruco
+Descripcion: Generador de numeros aleatorios
+Fecha: 2024-01-17
+					ANALISIS
+DEFINICION DEL PROBLEMA : crear un programa que muestre numeros aleatorios en la cual se pueda elegir
+la cantidad de numeros que se desea generar
+
+DATOS DE ENTRADA : se ingresara la cantidad de numeros que se desee generar 
+
+INFORMACION DE SALIDA : se imprimira en pantalla los numeros al azar generados 
+
+VARIABLES : Se hara uso de las siguientes variables de tipo int o entero cantidadNumeros,numeroAleatorio.
+
+					DISEÑO	
+
+Se devidira el problemas en pequeños problemas para resolver paso a paso
+
+- se hara uso de las librerias #include <stdlib.h>,#include <time.h> las cuales son necesarias para generar numeros aleatorios
+
+-se declaran variables de tipo int las cuales funcionaran como almacenamiento del numero agregado por el usuario, como contador, como resultado
+
+-se mostrara en pantalla que el usuario ingrese la cantidad de numeros que desea generar al azar, la cual se guardara en la variable cantidadNumeros
+
+-se hara uso de srand(time(0));la cual genera numeros al azar en tiempo real//es decir que cambiaran cada vez que se empiece a correr el programa
+
+-se hara uso del ciclo repetitivo for en la cual se utilizara la variable "i" como un contador la cual iniciara de 0 hasta la variable cantidadNumeros
+
+-la variable numeroAleatorio seran los datos aleatorios almacenados en dicha variable
+
+-para finalizar se imprimira en pantalla los numeros aleatorios generados 
+*/
+#include <iostream>
+#include <stdlib.h>  // Necesario para la función rand()
+#include <time.h>    // Necesario para la función time()
+
+using namespace std;
+
+int main() {
+	/* La función srand establece el punto de partida para generar una serie de enteros pseudoaleatorios en el subproceso actual
+	   time(0) proporciona un valor que cambia con el tiempo, asegurando que la secuencia de números aleatorios sea diferente en cada ejecución del programa.
+	*/
+	srand(time(0));
+	
+	int cantidadNumeros,numeroAleatorio,i;//variable en la que se almacena la cantidad de numeros que se generaran
+	
+	// Solicitar al usuario cuántos números aleatorios desea generar
+	cout << "Ingrese la cantidad de numeros aleatorios a generar: ";
+	cin >> cantidadNumeros;
+	
+	/* la variable numeroAleatorio: se inicializa con un valor obtenido mediante la función rand(),
+	que genera un número aleatorio, y se utiliza el operador % para limitar el rango a valores entre 0 y 99.
+	rand : recupera los números pseudaleatorios que se generan. Si se llama a rand antes de hacer ninguna llamada a srand
+	se genera la misma secuencia que si se llama a srand con seed con el valor 1--> es decir que los valores ya generados
+	por la primera inicializacion pueden volver a repetirse cada vez que corra el programa. 
+	*/
+	cout << "Numeros aleatorios generados:" << endl;
+	for (i = 0; i < cantidadNumeros; i++) 
+	{
+		numeroAleatorio = rand() % 100;  
+		cout << numeroAleatorio << " ";
+	}
+	
+	cout << endl;
+	
+	return 0;
+}
+
---
+++ tabla_de_multiplicar.cpp
@@ -0,0 +1,51 @@
+/*Titulo :ejercicio en clases	
+Autor : Ricardo flores soruco
+Descripcion: Tabla de multiplicar
+Fecha: 2024-01-17
+				ANALISIS
+DEFINICION DEL PROBLEMA : crear un programa que muestre la tabla de multiplicar la cual
+el usuario ingresara que tabla desea generar.
+
+DATOS DE ENTRADA : se ingresara la tabla que desee visualizar el usuario y se almacenara en
+la variable numero como int-
+
+INFORMACION DE SALIDA : se imprimira en pantalla la tabla generada escogida por el usuario
+
+VARIABLES : Se hara uso de las siguientes variables de tipo int o entero, numero,i,resultado
+
+					DISEÑO
+Se devidira el problemas en pequeños problemas para resolver paso a paso
+-se declaran variables de tipo int las cuales funcionaran como almacenamiento del numero agregado por el usuario, como contador, como resultado
+-se mostrara en pantalla que el usuario ingrese la tabla que desea visualizar, la cual se guardara en la variable numero
+-se mostrara en pantalla la tabla de multiplicar que se genero
+-se hara uso del ciclo repetitivo for en la cual se utilizara la variable "i" como un contador la cual iniciara de 1 hasta 10 e ira aumentando de 1 en 1
+-la variable resultado sera aquel resultado que se va a ir multiplicando por el valor ingresado por el usuario
+-para finalizar se imprimira en pantalla la tabla de multiplicar generada en columnas.
+*/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int numero,i,resultado;
+	
+	// El usuario ingresara la tabla que desea visualizar 
+	cout << "Ingrese la tabla que desea visualizar ";
+	cin >> numero;
+	
+	// Con el numero ingresado se generara la tabla de multiplicar
+	cout << "Tabla de multiplicar del " << numero << ":" << endl;
+	/* For = ciclo repetitivo 
+		i  = funciona como contador la cual en cada vuelta que de en el ciclo aumentara de 1 en 1 
+		la cual empezara de 1 hasta 10 y se aumetaran en cada ciclo mas 1 por ende se muestra i++
+		
+	*/
+	for (i = 1; i <= 10; i++)//ciclo repetitivo for/ se puede usar ; para separar en argumento  
+	{ 
+		resultado = numero * i;// esta linea es el resultado de la multiplicacion la cual se multiplicara de acuerdo al numero ingresado por el usuario
+		cout << numero << " x " << i << " = " << resultado << endl;
+	}
+	
+	return 0;
+}
+
